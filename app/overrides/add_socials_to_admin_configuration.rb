8f0eae58870f8872a4fa9f5b185237187f465bff
Javier Botero <javierbotero10273@gmail.com>
2025-07-31 11:58:15 -0500

    Improves selector for deface in settings

--- app/overrides/add_socials_to_admin_configuration.rb
+++ app/overrides/add_socials_to_admin_configuration.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_main_menu.html.erb',
   name: 'add_social_providers_link_configuration_menu',
-  insert_after: '[class="nav nav-sidebar"]',
-  text: '<ul class="nav nav-sidebar" id="sidebarSPREE_SOCIAL"><li class="sidebar-menu-item d-block w-100 text-muted"><%= configurations_sidebar_menu_item Spree.t(:social_authentication_methods), spree.admin_authentication_methods_path %></li></ul>'
+  insert_after: 'li.sidebar-menu-item.d-block.w-100.position-relative',
+  text: '<li class="sidebar-menu-item d-block w-100 text-muted"><%= configurations_sidebar_menu_item Spree.t(:social_authentication_methods), spree.admin_authentication_methods_path %></li>'
 )
