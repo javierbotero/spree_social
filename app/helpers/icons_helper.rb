e0b452d117095f02d1df9c6926e69ca3c585d0cd
Javier Botero <javierbotero10273@gmail.com>
2025-08-08 14:17:11 -0500

    Adds twitter

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -1,6 +1,7 @@
 module IconsHelper
   ICONS = {
-    'google_oauth2' => '<i class="fa-brands fa-google"></i>'
+    'google_oauth2' => '<i class="fa-brands fa-google"></i>',
+    'twitter' => '<i class="fa-brands fa-twitter"></i>'
   }
 
   def icon_by_provider(name)
